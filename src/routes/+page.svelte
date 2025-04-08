86d00bee58a0d3444b243e4b7a8282453cb5821d
Guilherme Carvalho <cguilherme182@gmail.com>
2025-04-08 12:31:01 -0300

    lab 7

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,22 @@
 <script>
-    import projects from "$lib/projects.json";
-    import Project from "$lib/Project.svelte";
-    let profileData = fetch("https://api.github.com/users/carvguilherme8");
+import projects from "$lib/projects.json";
+import Project from "$lib/Project.svelte";
+import { onMount } from "svelte";
+let profileData = fetch("https://api.github.com/users/carvguilherme8");
+
+let githubData = null;
+let loading = true;
+let error = null;
+
+onMount(async () => {
+    try {
+        const response = await fetch("https://api.github.com/users/carvguilherme8");
+        githubData = await response.json();
+    } catch (err) {
+        error = err;
+    }
+    loading = false;
+});
 </script>
 
 <div id="title">
@@ -15,29 +30,23 @@
     <p>- Badminton Open Rio Champion</p>
 </div>
 
-{#await fetch("https://api.github.com/users/carvguilherme8")}
-  <p>Loading...</p>
-{:then response}
-  {#await response.json()}
-    <p>Decoding...</p>
-  {:then data} 
+{#if loading}
+    <p>Loading...</p>
+{:else if error}
+    <p class="error">Something went wrong: {error.message}</p>
+{:else}
     <section>
-      <h2>My Github Stats</h2>
-      <dl>
-        <dt>Followers</dt>
-        <dd>{data.followers}</dd>
-        <dt>Following</dt>
-        <dd>{data.following}</dd>
-        <dt>Public Repos</dt>
-        <dd>{data.public_repos}</dd>
-      </dl>
+        <h2>My GitHub Stats</h2>
+        <dl>
+            <dt>Followers</dt>
+            <dd>{githubData.followers}</dd>
+            <dt>Following</dt>
+            <dd>{githubData.following}</dd>
+            <dt>Public Repositories</dt>
+            <dd>{githubData.public_repos}</dd>
+        </dl>
     </section>
-  {:catch error}
-    <p class="error">Something went wrong: {error.message}</p>
-  {/await}
-  {:catch error}
-    <p class="error">Something went wrong: {error.message}</p>
-{/await}
+{/if}
 
 
 <div class="projects">
